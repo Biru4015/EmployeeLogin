3de3e1f2e84955c9c48e46c24b701db7ce05a62f
Biru4015 <rambirendra12@gmail.com>
2020-05-28 23:11:48 +0530

    [Birendra]Refactor refactored. adding stylecop in UserDataDbContext class.

--- GreetingAppRepositoryLayer/Context/UserDatabaseContext.cs
+++ GreetingAppRepositoryLayer/Context/UserDatabaseContext.cs
@@ -1,16 +1,27 @@
-﻿using GreetingAppCommonLayer;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GreetingAppRepositoryLayer.Context
+﻿namespace GreetingAppRepositoryLayer.Context
 {
+    using GreetingAppCommonLayer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// This clas contains the code for User data base context.
+    /// </summary>
     public class UserDatabaseContext : DbContext
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDatabaseContext"/> class.
+        /// </summary>
+        /// <param name="options">The UserDatabaseContext</param>
         public UserDatabaseContext(DbContextOptions<UserDatabaseContext> options) : base(options)
         {
         }
+
+        /// <summary>
+        /// Gets and sets the Employee
+        /// </summary>
         public DbSet<GreetingModel> Employee { get; set; }
     }
 }
