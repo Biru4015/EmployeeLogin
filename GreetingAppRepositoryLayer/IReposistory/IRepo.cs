fc431f7591e0b69842f065054bc9c9380c8b2ce0
Biru4015 <rambirendra12@gmail.com>
2020-05-28 23:02:40 +0530

    [Birendra]Refactor refactored. adding stylecop in EmployeeRepository and IRepo interface.

--- GreetingAppRepositoryLayer/IReposistory/IRepo.cs
+++ GreetingAppRepositoryLayer/IReposistory/IRepo.cs
@@ -5,16 +5,44 @@
     using System.Collections.Generic;
     using System.Text;
     using System.Threading.Tasks;
+
+    /// <summary>
+    /// This is interface for EmployeeRepository.
+    /// </summary>
     public interface IRepo
     {
+        /// <summary>
+        ///  Adds the specified employee
+        /// </summary>
+        /// <param name="employee">The employee</param>
+        /// <returns>employee id</returns>
         Task<int> AddEmployee(GreetingModel employee);
-        
+
+        /// <summary>
+        ///  Getss the specified employee
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns>employee id</returns>
         GreetingModel GetEmployee(int id);
 
+        /// <summary>
+        ///  Updates the specified employee
+        /// </summary>
+        /// <param name="employeeChanges">The employeeChanges</param>
+        /// <returns>updated employee</returns>
         Task<int> UpdateEmployee(GreetingModel employeeChanges);
-        
+
+        /// <summary>
+        ///  Deletes the specified employee
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns>employee id</returns>
         GreetingModel DeleteEmployee(int id);
-        
+
+        /// <summary>
+        ///  Gets all employees
+        /// </summary>
+        /// <returns>list of all employees</returns>
         IEnumerable<GreetingModel> GetAllEmployees();
     }
 }
--- GreetingAppRepositoryLayer/Repository/EmployeeRepository.cs
+++ GreetingAppRepositoryLayer/Repository/EmployeeRepository.cs
@@ -7,26 +7,53 @@
     using System.Collections.Generic;
     using System.Text;
     using System.Threading.Tasks;
+
+    /// <summary>
+    /// This class is contains the code for EmployeeRepository class
+    /// </summary>
     public class EmployeeRepository:IRepo
     {
+        /// <summary>
+        /// The userDbContext object
+        /// </summary>
         private readonly UserDatabaseContext userDbContext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeRepository"/> class.
+        /// </summary>
+        /// <param name="userDbContext">The userDbContext</param>
         public EmployeeRepository(UserDatabaseContext userDbContext)
         {
             this.userDbContext = userDbContext;
         }
 
+        /// <summary>
+        /// Adding the specified employee details
+        /// </summary>
+        /// <param name="employee">The employee</param>
+        /// <returns></returns>
         public Task<int> AddEmployee(GreetingModel employee)
         {
             userDbContext.Employee.Add(employee);
             var result = userDbContext.SaveChangesAsync();
             return result;
         }
+
+        /// <summary>
+        /// Gets the specified employee
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns>id</returns>
         public GreetingModel GetEmployee(int id)
         {
             return userDbContext.Employee.Find(id);
         }
 
+        /// <summary>
+        /// Updates the specified employee
+        /// </summary>
+        /// <param name="employeeChanges">The employeeChanges</param>
+        /// <returns>Updated id</returns>
         public Task<int> UpdateEmployee(GreetingModel employeeChanges)
         {
             var employee = userDbContext.Employee.Attach(employeeChanges);
@@ -35,6 +62,11 @@
             return result;
         }
 
+        /// <summary>
+        /// Deletes the specified employee
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns>Deleted Employee</returns>
         public GreetingModel DeleteEmployee(int id)
         {
             GreetingModel employee = userDbContext.Employee.Find(id);
@@ -46,6 +78,10 @@
             return employee;
         }
 
+        /// <summary>
+        /// Gets all employees
+        /// </summary>
+        /// <returns>Details of employees</returns>
         public IEnumerable<GreetingModel> GetAllEmployees()
         {
             return userDbContext.Employee;
