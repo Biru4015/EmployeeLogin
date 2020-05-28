ff804d2a57fee09357ce6c0d3ae80a4fdd8cb6ef
Biru4015 <rambirendra12@gmail.com>
2020-05-28 23:28:32 +0530

    [Birendra]Refactor refactored. adding stylecop in EmployeeManager and IEmpolyeeManager interface.

--- GreetingAppManagerLayer/IManager/IEmployeeManager.cs
+++ GreetingAppManagerLayer/IManager/IEmployeeManager.cs
@@ -5,16 +5,44 @@
     using System.Collections.Generic;
     using System.Text;
     using System.Threading.Tasks;
+
+    /// <summary>
+    /// This is an interface for EmployeeManager
+    /// </summary>
     public interface IEmployeeManager
     {
+        /// <summary>
+        /// Adding the specified employee
+        /// </summary>
+        /// <param name="employee">The employee</param>
+        /// <returns>The employee</returns>
         Task<int> AddEmployee(GreetingModel employee);
-        
+
+        /// <summary>
+        /// Getting the specified employee
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns>The employee id</returns>
         GreetingModel GetEmployee(int id);
-        
+
+        /// <summary>
+        /// Updates the specified employee
+        /// </summary>
+        /// <param name="employeeChanges"></param>
+        /// <returns>The employee</returns>
         Task<int> UpdateEmployee(GreetingModel employeeChanges);
-        
+
+        /// <summary>
+        /// This is for deleted specified id employee.
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns>The employee id</returns>
         GreetingModel DeleteEmployee(int id);
-        
+
+        /// <summary>
+        ///  This is for getting all the employee details
+        /// </summary>
+        /// <returns>List of all employees</returns>
         IEnumerable<GreetingModel> GetAllEmployees();
     }
 }
--- GreetingAppManagerLayer/ManagerImplimentation/EmployeeManager.cs
+++ GreetingAppManagerLayer/ManagerImplimentation/EmployeeManager.cs
@@ -7,35 +7,70 @@
     using System.Text;
     using System.Threading.Tasks;
     using GreetingAppManagerLayer.IManager;
+
+    /// <summary>
+    /// This class contains the code for EmployeeManager.
+    /// </summary>
     public class EmployeeManager : IEmployeeManager
     {
+        /// <summary>
+        /// This method is instance for IRepo.
+        /// </summary>
         private readonly IRepo repository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeManager"/> class.
+        /// </summary>
+        /// <param name="repository">The repository</param>
         public EmployeeManager(IRepo repository)
         {
             this.repository = repository;
         }
 
+        /// <summary>
+        /// Adding the employee details for specified employee.
+        /// </summary>
+        /// <param name="employee">The employee</param>
+        /// <returns></returns>
         public Task<int> AddEmployee(GreetingModel employee)
         {
             return this.repository.AddEmployee(employee);
         }
 
+        /// <summary>
+        /// Getting the details for specified employee.
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns></returns>
         public GreetingModel GetEmployee(int id)
         {
             return this.repository.GetEmployee(id);
         }
 
+        /// <summary>
+        /// Updating the details of employee.
+        /// </summary>
+        /// <param name="employeeChanges">The employeeChanges</param>
+        /// <returns></returns>
         public Task<int> UpdateEmployee(GreetingModel employeeChanges)
         {
             return this.repository.UpdateEmployee(employeeChanges);
         }
 
+        /// <summary>
+        /// This method is for deleting the employee id.
+        /// </summary>
+        /// <param name="id">The id</param>
+        /// <returns></returns>
         public GreetingModel DeleteEmployee(int id)
         {
             return this.repository.DeleteEmployee(id);
         }
 
+        /// <summary>
+        /// This method is for getting employee details.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<GreetingModel> GetAllEmployees()
         {
             return this.repository.GetAllEmployees();
