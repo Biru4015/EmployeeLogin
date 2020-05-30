56fe93f5e13279b928e2af43ca9ea99277944204
Biru4015 <rambirendra12@gmail.com>
2020-05-30 13:51:40 +0530

    [Birendra]Add added. login.html file in wwwroot folder

--- GreetingApp/Program.cs
+++ GreetingApp/Program.cs
@@ -1,27 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Serilog;
-
-namespace GreetingApp
+﻿namespace GreetingApp
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Serilog;
     public class Program
     {
+        /// <summary>
+        /// This is main method of project
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-            Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.File(@"C:\Users\Birendra Kumar\source\repos\LoggerText.txt")
-            .CreateLogger();
         }
 
+        /// <summary>
+        /// This method is created for CreateWebHostBuilder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
